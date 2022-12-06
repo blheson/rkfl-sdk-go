a418ca743e776d6488f1ca7f8fea6c8b24b1b8f5
Udor Blessingm <berachah54@gmail.com>
2022-12-06 12:18:20 +0100

    remove password entirely

--- authorization.go
+++ authorization.go
@@ -15,23 +15,13 @@ type AuthorizationRequest struct {
 	Password string `json:"password"`
 }
 
-func (s *AuthorizationService) LoginWithPassword() (Response, error) {
-	u := fmt.Sprintf(baseAuth + "/login")
-	resp := Response{}
-
-	cred := s.client.getMerchantCred()
-
-	err := s.client.Call("POST", u, cred, &resp)
-
-	return resp, err
-}
 func (s *AuthorizationService) Login() (Response, error) {
 	u := fmt.Sprintf(baseAuth + "/generate-auth-token")
 	resp := Response{}
-	
+
 	cred := s.client.getMerchantCred()
 
-fmt.Println(cred,"Cred: Testing init")
+	fmt.Println(cred, "Cred: Testing init")
 
 	err := s.client.Call("POST", u, cred, &resp)
 
